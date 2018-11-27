d7c80e0cc2581cc8b0f85452fc998adbb35aba45
tiwarip1 <herpitehderpo@gmail.com>
2018-11-27 17:07:04 -0500

    input files ready to be run

---
+++ run.sh
@@ -0,0 +1,3 @@
+~/Desktop/QE/q-e-qe-5.2.0/bin/pw.x<Ru327.scf.in>Ru327.scf.out
+~/Desktop/QE/q-e-qe-5.2.0/bin/pw.x<Ru327.nscf.in>Ru327.nscf.out
+~/Desktop/QE/q-e-qe-5.2.0/bin/bands.x<bands.in>bands.out
